d39d658e45b1051663db3629702b956d971a5bd7
Jess Tapley <jjtapley@aol.com>
2020-09-28 16:44:12 +0100

    Basic styling completed with grid layout and some basic images, etc. The Office font added to page

--- collection.php
+++ collection.php
@@ -17,13 +17,20 @@ $quotes = $query->fetchAll();
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>The Office US Sitcom Quotes Collection</title>
         <link type="text/css" rel="stylesheet" href="normalize.css">
+        <link type="text/css" rel="stylesheet" href="styling.css">
     </head>
 
     <body>
-        <div>
-            <h1>The Office US Sitcom - Quotes Collection</h1>
+        <img class = "headerImage" src="titleImage.png" alt="The Office sitcom logo"/>
+        <div class="header">
+            <h1>US Sitcom</h1>
+            <h1>Quotes Collection</h1>
         </div>
-        <?php displayDBContent($quotes);?>
+        <img class = "castPic" src="background.jpg" alt="picture of the cast of The Office"/>
+        <div class = "grids">
+            <?php displayDBContent($quotes);?>
+        </div>
+
     </body>
 </html>
 
--- displayDBContentFunction.php
+++ displayDBContentFunction.php
@@ -2,18 +2,28 @@
 
 function displayDBContent($quotes) {
     foreach ($quotes as $field) {
-        echo '<div>';
-        echo '<img src="' . $field['url'] . '"/>';
-        echo '</div>';
-        echo '<div>';
-        echo '<h3>Quote:</h3>';
-        echo '<p>' . $field['quote'] . '</p>';
-        echo '<h3>Who Said It:</h3>';
-        echo '<p>' . $field['whoSaidIt'] . '</p>';
-        echo '<h3>Episode:</h3>';
-        echo '<p>' . $field['episode'] . '</p>';
-        echo '<h3>Hilarity-O-Meter:</h3>';
-        echo '<p>' . $field['hilarity-ometer'] . '</p>';
+        echo '<div class="displayDB">';
+            echo '<div class="images">';
+                echo '<img src="' . $field['url'] . '"/>';
+            echo '</div>';
+            echo '<div class="collectionItems">';
+                echo '<div>';
+                echo '<h3>Quote:</h3>';
+                echo '<p>' . $field['quote'] . '</p>';
+            echo '</div>';
+            echo '<div>';
+                echo '<h3>Who Said It:</h3>';
+                echo '<p>' . $field['whoSaidIt'] . '</p>';
+            echo '</div>';
+            echo '<div>';
+                echo '<h3>Episode:</h3>';
+                echo '<p>' . $field['episode'] . '</p>';
+            echo '</div>';
+            echo '<div>';
+                echo '<h3>Hilarit-O-Meter:</h3>';
+                echo '<p>' . $field['hilarity-ometer'] . '</p>';
+                echo '</div>';
+            echo '</div>';
         echo '</div>';
     }
 }
