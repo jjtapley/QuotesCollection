6a152144fa7830640f5bfd56029a889fa92506ec
Jess Tapley <jjtapley@aol.com>
2020-09-28 14:53:00 +0100

    Initial commit - created database connection and query - adding all image files to repo - setting up basic HTML

---
+++ collection.php
@@ -0,0 +1,29 @@
+
+<?php
+
+$db = new PDO ('mysql:host=db; dbname=OfficeQuotesDB', 'root', 'password');
+$db -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+$query = $db->prepare('SELECT * FROM `quotes` INNER JOIN `images` ON `quotes`.`id` = `images`.`quotes-ID`');
+$query->execute();
+$quotes = $query->fetchAll();
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset ="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>The Office US Sitcom Quotes Collection</title>
+        <link type="text/css" rel="stylesheet" href="normalize.css">
+    </head>
+
+    <body>
+        <div>
+            <h1>The Office US Sitcom - Quotes Collection</h1>
+        </div>
+        <?php displayDBContent($quotes);?>
+    </body>
+</html>
+
